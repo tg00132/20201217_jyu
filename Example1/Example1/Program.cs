51f416c6c5ab8cadb3f1306b8480a22a99d766b7
tg00132 <75109813+tg00132@users.noreply.github.com>
2020-12-17 16:37:16 +0900

    visual20201217

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+            //java: system.out.println("Hello World");
+            //c# :Console.WriteLine("Hello World");
+          Console.WriteLine("hello world");
+            
+            int a = 100;
+            if (a > 50)
+            {
+                Console.WriteLine("a는 50을 넘어요.");
+            }
+            else
+            {
+                Console.WriteLine("a는 50을 넘지 않아요.");
+            }
+            //java
+
+            // Scanner s=new Scanner(System.in);
+            //  string ss = s.nextint();
+            // int aa = s.nextint();
+            string ss = Console.ReadLine();
+            String sa = Console.ReadLine();
+            int aa = int.Parse(Console.ReadLine());
+            switch (aa)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            for (int i = 0; i<10; i++)
+            {
+
+            }
+            int temp = 10;
+            while (temp >0)
+            {
+                temp--;
+            }
+            do
+            {
+                temp++;
+            } while (temp < 0);
+
+            int abc = 100;
+            if (abc == 100)
+            {
+
+            }
+            else if(abc ==50)
+            {
+
+            }
+            else
+            {
+
+            }
+        
+        }
+    }
+
+    
+    }
+
